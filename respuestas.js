40c2bb0fce56cff85221dbb618ca450fbc02e996
Xavi <xavipgweb@gmail.com>
2016-06-07 20:25:08 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,6 +1,7 @@
  var paragraph1 = document.getElementById("number1");
  var paragraph2 = document.getElementById("number2");
  var paragraph3 = document.getElementById("number3");
+ var element = document.getElementById("text");
  
   function ok(){
   var rnd = Math.floor((Math.random() * 3) + 1);
