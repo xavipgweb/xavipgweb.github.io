c3dddeae83f81cac487faa9c82464acd438ad5f3
Xavi <xavipgweb@gmail.com>
2016-06-07 19:08:34 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,5 +1,13 @@
-
+ var element = document.getElementById("text");
   function myFunction(){
-  var element = document.getElementById("text");
-  element.innerHTML = "New Header";
+  var rnd = Math.floor((Math.random() * 3) + 1);
+  if (rnd == 1){
+  element.innerHTML = "1";
   }
+  if (rnd == 2){
+  element.innerHTML = "2";
+  }
+  if (rnd == 3){
+  element.innerHTML = "3";
+  }
+}
