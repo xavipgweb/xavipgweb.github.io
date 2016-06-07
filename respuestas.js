3864dc46be4081d83c6e716ce2b2865bbc43c5f0
Xavi <xavipgweb@gmail.com>
2016-06-07 21:10:28 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -20,19 +20,19 @@
    var rnd = Math.floor((Math.random() * 3) + 1);
    if (rnd == 1){
     element.innerHTML = "You are in a forest. There is an axe near you";
-    paragraph1.innerHTML = "Take a walk";
+    number1.innerHTML = "Take a walk";
     paragraph2.innerHTML = "Wait a few minutes";
     paragraph3.innerHTML = "Grab the axe";
    }
    if (rnd == 2){
     element.innerHTML = "You are at the beach. There is a hill";
-    paragraph1.innerHTML = "Swim";
+    number1.innerHTML = "Swim";
     paragraph2.innerHTML = "Run to the hill";
     paragraph3.innerHTML = "Wait a few minutes";
    }
    if (rnd == 3){
     element.innerHTML = "You are at the Moon. There is a rover";
-    paragraph1.innerHTML = "Drive the rover";
+    nnumber1.innerHTML = "Drive the rover";
     paragraph2.innerHTML = "Try to communicate";
     paragraph3.innerHTML = "Take a walk";
    }
