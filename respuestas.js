5d0e410761b1483003ee34be3ccaa0a666398c43
Xavi <xavipgweb@gmail.com>
2016-06-07 19:17:42 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -2,17 +2,21 @@
   function myFunction(){
   var rnd = Math.floor((Math.random() * 3) + 1);
   if (rnd == 1){
-  element.innerHTML = "1";
+  element.innerHTML = "4";
   }
   if (rnd == 2){
-  element.innerHTML = "2";
+  element.innerHTML = "5";
   }
   if (rnd == 3){
-  element.innerHTML = "3";
+  element.innerHTML = "6";
   }
 }
 
   function start(){
+   
+   var x = document.createElement("INPUT");
+   x.setAttribute("type", "text");
+   
    var rnd = Math.floor((Math.random() * 3) + 1);
    if (rnd == 1){
    element.innerHTML = "You are in a forest";
