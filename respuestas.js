bd98428d9e01ed673c8b72ef640174df65c34a16
Xavi <xavipgweb@gmail.com>
2016-06-07 20:24:27 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,15 +1,17 @@
- var element = document.getElementById("text");
- var element = document.getElementById("text");
+ var paragraph1 = document.getElementById("number1");
+ var paragraph2 = document.getElementById("number2");
+ var paragraph3 = document.getElementById("number3");
+ 
   function ok(){
   var rnd = Math.floor((Math.random() * 3) + 1);
   if (rnd == 1){
-  element.innerHTML = "1";
+  number1.innerHTML = "1";
   }
   if (rnd == 2){
-  element.innerHTML = "2";
+  number2.innerHTML = "2";
   }
   if (rnd == 3){
-  element.innerHTML = "3";
+  number3.innerHTML = "3";
   }
 }
 
