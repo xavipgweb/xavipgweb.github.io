d97540dff7d26669a7b964143068d1db6c69ab43
Xavi <xavipgweb@gmail.com>
2016-06-07 19:11:34 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -11,3 +11,16 @@
   element.innerHTML = "3";
   }
 }
+
+  function start(){
+   var rnd = Math.floor((Math.random() * 3) + 1);
+   if (rnd == 1){
+   element.innerHTML = "You are in a forest";
+   }
+   if (rnd == 2){
+   element.innerHTML = "You are at the beach";
+   }
+   if (rnd == 3){
+   element.innerHTML = "You are at the Moon";
+   }
+  }
