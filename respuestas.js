74d974fd35a688a3141298f98029c3868575921e
Xavi <xavipgweb@gmail.com>
2016-06-07 21:07:53 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -4,27 +4,36 @@
  var element = document.getElementById("text");
  
   function ok(){
-  var rnd = Math.floor((Math.random() * 3) + 1);
-  if (rnd == 1){
-  number1.innerHTML = "1";
-  }
-  if (rnd == 2){
-  number2.innerHTML = "2";
-  }
-  if (rnd == 3){
-  number3.innerHTML = "3";
-  }
-}
+   var rnd = Math.floor((Math.random() * 3) + 1);
+   if (rnd == 1){
+    paragraph1.innerHTML = "1";
+   }
+   if (rnd == 2){
+    paragraph2.innerHTML = "2";
+   }
+   if (rnd == 3){
+    paragraph3.innerHTML = "3";
+   }
+ }
 
   function start(){
    var rnd = Math.floor((Math.random() * 3) + 1);
    if (rnd == 1){
-   element.innerHTML = "You are in a forest";
+    element.innerHTML = "You are in a forest. There is an axe near you";
+    paragraph1.innerHTML = "Take a walk";
+    paragraph2.innerHTML = "Wait a few minutes";
+    paragraph3.innerHTML = "Grab the axe";
    }
    if (rnd == 2){
-   element.innerHTML = "You are at the beach";
+    element.innerHTML = "You are at the beach. There is a hill";
+    paragraph1.innerHTML = "Swim";
+    paragraph2.innerHTML = "Run to the hill";
+    paragraph3.innerHTML = "Wait a few minutes";
    }
    if (rnd == 3){
-   element.innerHTML = "You are at the Moon";
+    element.innerHTML = "You are at the Moon. There is a rover";
+    paragraph1.innerHTML = "Drive the rover";
+    paragraph2.innerHTML = "Try to communicate";
+    paragraph3.innerHTML = "Take a walk";
    }
   }
