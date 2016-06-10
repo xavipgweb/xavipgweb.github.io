901b9ccd12fc124699188a4bc60510c94037878f
Xavi <xavipgweb@gmail.com>
2016-06-10 19:48:11 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -17,20 +17,5 @@
  }
 
   function start(){
-   var rnd = Math.floor((Math.random() * 3) + 1);
-   if (rnd == 1){
-    element.innerHTML = "You are in a forest. There is an axe near you";
-    paragraph1.textContent = "Take a walk";
-    
-   }
-   if (rnd == 2){
-    element.innerHTML = "You are at the beach. There is a hill";
-    paragraph1.textContent = "Swim";
-   
-   }
-   if (rnd == 3){
-    element.innerHTML = "You are on the Moon. There is a rover";
-    paragraph1.textContent = "Drive the rover";
-    
-   }
+   paragraph1.textContent = "Swim";
   }
