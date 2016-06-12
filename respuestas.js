e6ee6ba493ad9c19cd48c7d76e2f0d12907dd70f
Xavi <xavipgweb@gmail.com>
2016-06-12 18:03:08 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,7 +1,9 @@
- var par_one = document.getElementById("paruno");
- var element = document.getElementById("text");
+
  
-  function start(){
-   document.getElementById("paruno").innerHTML = "Swim.";
-   document.getElementById("text").innerHTML = "Hi";
+  function ok(){
+    var par_one = document.getElementById("paruno");
+    var element = document.getElementById("text");
+    
+    par_one.innerHTML = "Walk";
+    element.innerHTML = "Hi";
   }
