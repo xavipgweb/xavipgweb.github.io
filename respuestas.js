0b560933a6e30aa504043a57db9609f4f0b567f5
Xavi <xavipgweb@gmail.com>
2016-06-12 17:57:52 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,21 +1,6 @@
- var paragraph1 = document.getElementById("paruno");
- var paragraph2 = document.getElementById("pardos");
- var paragraph3 = document.getElementById("partres");
+ var par_one = document.getElementById("paruno");
  var element = document.getElementById("text");
  
-  function ok(){
-   var rnd = Math.floor((Math.random() * 3) + 1);
-   if (rnd == 1){
-    paragraph1.innerHTML = "1";
-   }
-   if (rnd == 2){
-    
-   }
-   if (rnd == 3){
-    
-   }
- }
-
   function start(){
-   paragraph1.textContent = "Swim";
+   par_one.innerHTML = "Swim";
   }
