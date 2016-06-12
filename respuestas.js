5e47cc7d9d6cb46008b03e272dca57a85fa31de9
Xavi <xavipgweb@gmail.com>
2016-06-12 18:10:17 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -2,8 +2,14 @@
  
   function start(){
    var par_one = document.getElementById("paruno");
+   var par_two = document.getElementById("partwo");
+   var par_three = document.getElementById("parthree");
+   
    var element = document.getElementById("text");
  
     par_one.innerHTML = "Grab the axe";
+    par_two.innerHTML = "Take a walk";
+    par_three.innerHTML = "Wait a few minutes";
+    
     element.innerHTML = "Bye";
   }
