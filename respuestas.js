820a4429b87e5580a4a0073becfe7c70714948a6
Xavi <xavipgweb@gmail.com>
2016-06-12 18:07:11 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,7 +1,9 @@
- var par_one = document.getElementById("paruno");
- var element = document.getElementById("text");
+ 
  
   function start(){
+   var par_one = document.getElementById("paruno");
+   var element = document.getElementById("text");
+ 
     par_one.innerHTML = "Grab the axe";
     element.innerHTML = "Bye";
   }
