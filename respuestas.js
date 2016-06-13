72d30dd0fd409ebfd8ac2242e4e1a7d293dd643e
Xavi <xavipgweb@gmail.com>
2016-06-13 22:48:22 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,11 +1,12 @@
  
- var rnd = Math.floor((Math.random() * 3) + 1);
- 
+
+ var rnd = Math.floor((Math.random() * 2) + 1);
+
   function start(){
    var par_one = document.getElementById("paruno");
    var par_two = document.getElementById("partwo");
    var element = document.getElementById("text");
-   
+
    if (rnd == 1){
    par_one.innerHTML = "Grab the axe";
    par_two.innerHTML = "Take a walk";
@@ -16,24 +17,42 @@
    par_two.innerHTML = "Go away";
    element.innerHTML = "You are at the beach.";
    }
-   if (rnd == 3){
-   par_one.innerHTML = "Take a walk";
-   par_two.innerHTML = "Wait a few minutes";
-   element.innerHTML = "You are on the Moon. There is a rover.";
-   }
   }
-  
+
   function ok(){
    var par_one = document.getElementById("paruno");
    var par_two = document.getElementById("partwo");
    var element = document.getElementById("text");
-   
+
+   var selection1;
+   var selection2;
+
    var rad_one = document.getElementById("rad1").checked;
    var rad_two = document.getElementById("rad2").checked;
-    
+
    if ((rnd == 1)&&(rad_one)){
     par_one.innerHTML = "Cut a tree";
     par_two.innerHTML = "Take a walk";
     element.innerHTML = "You grabbed the axe.";
-    }
+    selection1 = 1;
   }
+  if ((rnd == 1)&&(rad_two)){
+     par_one.innerHTML = "Continue walking";
+     par_two.innerHTML = "Run after it";
+     element.innerHTML = "You see a rabbit.";
+     selection1 = 2;
+  }
+
+  if ((rnd == 2)&&(rad_one)){
+      par_one.innerHTML = "Swim to it";
+      par_two.innerHTML = "Continue swimming";
+      element.innerHTML = "You see an island.";
+      selection2 = 1;
+  }
+  if ((rnd == 2)&&(rad_two)){
+      par_one.innerHTML = "Go inside the forest";
+      par_two.innerHTML = "Return to the beach";
+      element.innerHTML = "You see a forest.";
+      selection2 = 2;
+  }
+}
